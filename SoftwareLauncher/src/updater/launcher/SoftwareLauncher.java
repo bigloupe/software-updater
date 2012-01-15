61d3044d501789b86ed613a2c0ad6559064444fe
cws1989 <cws1989@gmail.com>
2012-01-15 08:00:27 +0000

    Changes: 1. allow revert/rollback the update no matter what kind of 'update failed' reason

--- SoftwareLauncher/src/updater/launcher/SoftwareLauncher.java
+++ SoftwareLauncher/src/updater/launcher/SoftwareLauncher.java
@@ -225,20 +225,18 @@ public class SoftwareLauncher {
 
         JOptionPane.showMessageDialog(updaterFrame, "Error occurred when updating the software.");
 
-        if (updaterGUI.isCancelEnabled()) {
-          Object[] options = {"Recover", "Exit & Restart manually"};
-          int result = JOptionPane.showOptionDialog(null, "Recover back to original version or exit & restart your computer manually?", "Update Failed", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[1]);
-          if (result == 0) {
-            try {
-              doRevert(client.getPatches(), new File(storagePath));
-            } catch (IOException ex1) {
-              JOptionPane.showMessageDialog(updaterFrame, "Error occurred when doing the revertion.");
-              LOG.log(Level.SEVERE, null, ex1);
-              return;
-            }
-          } else {
+        Object[] options = {"Recover", "Exit & Restart manually"};
+        int result = JOptionPane.showOptionDialog(null, "Recover back to original version or exit & restart your computer manually?", "Update Failed", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[1]);
+        if (result == 0) {
+          try {
+            doRevert(client.getPatches(), new File(storagePath));
+          } catch (IOException ex1) {
+            JOptionPane.showMessageDialog(updaterFrame, "Error occurred when doing the revertion.");
+            LOG.log(Level.SEVERE, null, ex1);
             return;
           }
+        } else {
+          return;
         }
       } finally {
         updaterFrame.setVisible(false);
